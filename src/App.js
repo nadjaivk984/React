ade585f0a132e528f99c9047e722377272f9f76c
nadjaivk984 <nadjaivkovic984@gmail.com>
2021-08-26 22:43:45 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import reactDom from 'react-dom';
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="app">
+//       <form>
+//         <h2>Login</h2>
+//      <input type="text" value="name" className="name" placeholder="username" />
+//      <input type="text" className="password" placeholder="password" />
+//      <button className="login" onClick={}>Login</button>
+//      <a href="#">reset</a>
+//      </form>
+//     </div>
+//   );
+// }
+
+
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state={
+      name:"",
+      password:"",
+    }
+
+
+   
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChange1 = this.handleChange1.bind(this);
+    this.alert1 = this.alert1.bind(this);
+    this.clear = this.clear.bind(this);
+  }
+  
+
+  handleChange(event) {
+    this.setState({name: event.target.value})
+     
+  }
+
+  handleChange1(event) {
+    this.setState({password: event.target.value})
+     
+  }
+  alert1(){
+
+   alert(this.state.name + this.state.password)
+
+
+  }
+
+  clear(){
+    this.setState({password: ""})
+    this.setState({name: ""})
+
+  }
+
+
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form>
+        <h2>Login</h2>
+     <input type="text" value={this.state.name}  onChange={this.handleChange} className="name" placeholder="username" />
+     <input type="text" value={this.state.password} className="password" placeholder="password" onChange={this.handleChange1}  />
+     <button className="login" onClick={this.alert1}>Login</button>
+     <a href="#" onClick={this.clear} >reset</a>
+     </form>
     </div>
   );
+  }
+
 }
 
 export default App;
